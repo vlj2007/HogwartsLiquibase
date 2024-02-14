51565d9653197401b60f456b4d3bdfcb5399d2aa
vlj2007 <vlj2007@yandex.ru>
2024-02-14 15:41:26 +0700

    I had edited two files: hogwarts.sql and application.properties

--- src/main/resources/liquibase/scripts/hogwarts.sql
+++ src/main/resources/liquibase/scripts/hogwarts.sql
@@ -2,7 +2,6 @@
 
 -- changeset vpotapov:1
 CREATE INDEX student_search_name_index ON student (name);
-SELECT * FROM student WHERE name='';
+
 -- changeset vpotapov:2
 CREATE INDEX faculty_search_name_and_color_index ON faculty (name, color);
-SELECT * FROM faculty WHERE name='' and color='';
